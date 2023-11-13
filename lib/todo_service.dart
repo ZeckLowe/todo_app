b41bdecbe4d06f8c1a3f51f0a86df4471411650b
Zeck Lowe <lowezeck@gmail.com>
2023-11-13 18:38:56 +0800

    created todo_item.dart

---
+++ lib/todo_service.dart
@@ -0,0 +1,29 @@
+import 'package:hive/hive.dart';
+import 'package:todo_app/todo_item.dart';
+
+class TodoService {
+  final String _boxName = "todoBox";
+
+  Future get _box async => await Hive.openBox<TodoItem>(_boxName);
+
+  Future<void> addItem(TodoItem todoItem) async {
+    var box = await _box;
+    await box.add(todoItem);
+  }
+
+  Future<List<TodoItem>> getAllTodos() async {
+    var box = await _box;
+    return box.values.toList();
+  }
+
+  Future<void> deleteTodo(int index) async {
+    var box = await _box;
+    await box.deleteAt(index);
+  }
+
+  Future<void> updateIsCompleted(int index, TodoItem todoItem) async {
+    var box = await _box;
+    todoItem.isCompleted = !todoItem.isCompleted;
+    await box.putAt(index, todoItem);
+  }
+}
